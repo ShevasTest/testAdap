e6d3c644b076c946185a4efb0aad3efaa575423d
ShevasTest <klukin2712@gmail.com>
2022-07-09 19:23:41 +0200

    file:update

--- travel/index.js
+++ travel/index.js
@@ -4,10 +4,19 @@ console.log("1) 10\n2) 20\n3) 48\n4) 12\n5) 20");
 const header_burgerItem = document.querySelector('.header_burger');
 const menu = document.querySelector('.header_mobile_burger');
 const menu_close = document.querySelector('.burger_close_js');
+const menuLinks = document.querySelectorAll('.header_mobile_link');
 header_burgerItem.addEventListener('click', () => {
 menu.classList.add('header_mobile_burger_active');
 });
 menu_close.addEventListener('click', () => {
  menu.classList.remove('header_mobile_burger_active');
 });
+if (window.innerWidth <= 391) {
+    for (let i = 0; i < menuLinks.length; i += 1) {
+        menuLinks[i].addEventListener('click', () => {
+            menu.classList.remove('header_mobile_burger_active');
+        });
+    }
+}
 }());
+
